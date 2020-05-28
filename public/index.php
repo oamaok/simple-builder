ca1ffbe8576d2260c82abb2e9d693b95bb8bbb0e
Teemu Pääkkönen <oamaok@gmail.com>
2020-05-28 11:23:02 +0300

    Use absolute paths

--- public/index.php
+++ public/index.php
@@ -1,22 +1,22 @@
 <?php
 
-require_once('../inc/hmac.php');
+require_once(dirname(__file__) . '/../inc/hmac.php');
 
-$config = json_decode(file_get_contents('../config.json'));
+$config = json_decode(file_get_contents(dirname(__file__) . '/../config.json'));
 
 if (isset($_GET['build'])) {
-  require_once('../inc/build.php');
+  require_once(dirname(__file__) . '/../inc/build.php');
   exit(0);
 }
 
 if (isset($_GET['status'])) {
-  require_once('../inc/status.php');
+  require_once(dirname(__file__) . '/../inc/status.php');
   exit(0);
 }
 
 if (isset($_GET['kill'])) {
-  require_once('../inc/kill.php');
+  require_once(dirname(__file__) . '/../inc/kill.php');
   exit(0);
 }
 
-require_once('../inc/index.php');
+require_once(dirname(__file__) . '/../inc/index.php');
