9991c6e617e213d789e2071ddd9a0b8c7a63e919
Teemu Pääkkönen <oamaok@gmail.com>
2020-05-28 12:49:35 +0300

    Add kill message to log

--- inc/kill.php
+++ inc/kill.php
@@ -3,3 +3,7 @@
 if (validate_token($_GET['token'], $payload)) die('Invalid token');
 
 exec('kill ' . $payload->pid);
+
+$f = fopen($config->log_dir . '/' . $payload->logfile, 'a');
+fwrite($f, "\n[BUILD KILLED]");
+fclose($f);
